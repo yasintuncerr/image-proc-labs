9cea61ed70c9eeb9bc36549e59ef0dc93e1c935a
yasin-ait <yasin@ait.com.tr>
2024-10-23 22:18:42 +0300

    Update Parallel methods removed

--- Chapters/ch2/model/quantization.go
+++ Chapters/ch2/model/quantization.go
@@ -1,182 +1,48 @@
 package model
 
 import (
-	"fmt"
+	"image"
 	"math"
-	"sync"
 )
 
-type BitLevel uint16
+func normalize(src []uint8, dst []float64, level int) {
 
-const (
-	Bit8 BitLevel = 256
-	Bit7 BitLevel = 128
-	Bit6 BitLevel = 64
-	Bit5 BitLevel = 32
-	Bit4 BitLevel = 16
-	Bit3 BitLevel = 8
-	Bit2 BitLevel = 4
-	Bit1 BitLevel = 2
-)
-
-func getMaxValue(data []uint8) uint16 {
-	var max uint16 = 0
-	for i := 0; i < len(data); i++ {
-		if uint16(data[i]) > max {
-			max = uint16(data[i])
-		}
-	}
-	return max
-}
-
-func DetectBitLevel(data []uint8) BitLevel {
-	maxValue := getMaxValue(data)
-	switch {
-	case maxValue < 2:
-		return Bit1
-	case maxValue < 4:
-		return Bit2
-	case maxValue < 8:
-		return Bit3
-	case maxValue < 16:
-		return Bit4
-	case maxValue < 32:
-		return Bit5
-	case maxValue < 64:
-		return Bit6
-	case maxValue < 128:
-		return Bit7
-	default:
-		return Bit8
+	for i, v := range src {
+		dst[i] = float64(v) / float64(level)
 	}
 }
 
-func NormalizeData(src []uint8, dst []float64, bitLevel BitLevel, start, length int) error {
-	if src == nil {
-		return fmt.Errorf("input array is nil")
-	}
-	if dst == nil {
-		return fmt.Errorf("output array is nil")
+func quantize(src []float64, dst []uint8, level int) {
+
+	for i, v := range src {
+		dst[i] = uint8(math.Floor(v * float64(level)))
 	}
 
-	for i := start; i < len(src) && i < start+length; i++ {
-		dst[i] = float64(src[i]) / float64(bitLevel)
-	}
-	return nil
 }
 
-func QuantizeData(src []float64, dst []uint8, bitLevel BitLevel, start, length int) error {
-	if src == nil {
-		return fmt.Errorf("input array is nil")
-	}
-	if dst == nil {
-		return fmt.Errorf("output array is nil")
-	}
+func ResampleImage(src, dst *image.Gray, srcBitLevel, dstBitLevel int) {
 
-	for i := start; i < len(src) && i < start+length; i++ {
-		dst[i] = uint8(math.Floor(float64(bitLevel) * src[i]))
-	}
-	return nil
-}
+	// Calculate the levels
+	srcLevel := int(math.Pow(2, float64(srcBitLevel)))
+	dstLevel := int(math.Pow(2, float64(dstBitLevel)))
 
-func ParallelNormalize(src []uint8, dst []float64, bitLevel BitLevel, concurrencyCount int) error {
-	if getMaxValue(src) > uint16(bitLevel) {
-		return fmt.Errorf("maximum value of the input array is greater than the bit level")
-	}
+	// allocate the normalized slices
+	src_norm := make([]float64, len(src.Pix))
+	dst_norm := make([]float64, len(dst.Pix))
 
-	var wg sync.WaitGroup
-	wg.Add(concurrencyCount)
+	// allocate the quantized slices
+	quantizedData := make([]uint8, len(src.Pix))
 
-	chunkSize := len(src) / concurrencyCount
-	for i := 0; i < concurrencyCount; i++ {
-		go func(i int) {
-			defer wg.Done()
-			start := i * chunkSize
-			end := start + chunkSize
-			if i == concurrencyCount-1 {
-				end = len(src)
-			}
-			NormalizeData(src, dst, bitLevel, start, end-start)
-		}(i)
-	}
-	wg.Wait()
+	//Quantization
 
-	return nil
-}
+	normalize(src.Pix, src_norm, srcLevel)
 
-func ParallelQuantize(src []float64, dst []uint8, bitLevel BitLevel, concurrencyCount int) error {
-	var wg sync.WaitGroup
-	wg.Add(concurrencyCount)
+	quantize(src_norm, quantizedData, dstLevel)
 
-	chunkSize := len(src) / concurrencyCount
-	for i := 0; i < concurrencyCount; i++ {
-		go func(i int) {
-			defer wg.Done()
-			start := i * chunkSize
-			end := start + chunkSize
-			if i == concurrencyCount-1 {
-				end = len(src)
-			}
-			QuantizeData(src, dst, bitLevel, start, end-start)
-		}(i)
-	}
-	wg.Wait()
+	// Dequantization
 
-	return nil
-}
+	normalize(quantizedData, dst_norm, dstLevel)
 
-func Resample(src []uint8, dst []uint8, bitLevel BitLevel) error {
-	if src == nil {
-		return fmt.Errorf("input array is nil")
-	}
-	if dst == nil {
-		return fmt.Errorf("output array is nil")
-	}
+	quantize(dst_norm, dst.Pix, srcLevel)
 
-	if len(src) != len(dst) {
-		return fmt.Errorf("input and output arrays have different lengths")
-	}
-
-	normalizedData := make([]float64, len(src))
-	if err := NormalizeData(src, normalizedData, bitLevel, 0, len(src)); err != nil {
-		return err
-	}
-
-	if err := QuantizeData(normalizedData, dst, bitLevel, 0, len(src)); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func ParallelResample(src []uint8, dst []uint8, bitLevel BitLevel, concurrencyCount int) error {
-	if getMaxValue(src) > uint16(bitLevel) {
-		return fmt.Errorf("maximum value of the input array is greater than the bit level")
-	}
-
-	var wg sync.WaitGroup
-	var err error
-	var mu sync.Mutex
-
-	wg.Add(concurrencyCount)
-	chunkSize := len(src) / concurrencyCount
-
-	for i := 0; i < concurrencyCount; i++ {
-		go func(i int) {
-			defer wg.Done()
-			start := i * chunkSize
-			end := start + chunkSize
-			if i == concurrencyCount-1 {
-				end = len(src)
-			}
-			if resampleErr := Resample(src[start:end], dst[start:end], bitLevel); resampleErr != nil {
-				mu.Lock()
-				err = resampleErr
-				mu.Unlock()
-			}
-		}(i)
-	}
-	wg.Wait()
-
-	return err
 }
