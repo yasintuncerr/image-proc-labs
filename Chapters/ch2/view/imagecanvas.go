be0f7d8f561fd1ab62fe6fda13605541836ecd3c
yasin-ait <yasin@ait.com.tr>
2024-10-23 22:15:28 +0300

    Update image canvas fill mod

--- Chapters/ch2/view/imagecanvas.go
+++ Chapters/ch2/view/imagecanvas.go
@@ -3,6 +3,7 @@ package view
 import (
 	"image"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
 )
@@ -10,11 +11,15 @@ import (
 type ImageCanvas struct {
 	ImgCanvas  *canvas.Image
 	TitleLabel *widget.Label
+	width      float32
+	height     float32
 }
 
-func NewImageCanvas(img *image.Gray, title string) *ImageCanvas {
+func NewImageCanvas(img *image.Gray, title string, width, height float32) *ImageCanvas {
 	imgCanvas := canvas.NewImageFromImage(img)
-	imgCanvas.FillMode = canvas.ImageFillOriginal
+	imgCanvas.FillMode = canvas.ImageFillContain
+	imgCanvas.SetMinSize(fyne.NewSize(width, height))
+
 	titleLabel := widget.NewLabel(title)
 	return &ImageCanvas{
 		ImgCanvas:  imgCanvas,
